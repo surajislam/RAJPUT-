38ff64a46ed224a0e4e418bfade49e9b272cee06
surajislam <154444093+surajislam@users.noreply.github.com>
2024-05-26 02:02:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 9999999999))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @EMMA_X_ROBOT on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 6528942884))
+OWNER_ID = int(getenv("OWNER_ID",  6528942884))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
