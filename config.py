86a970d111cd4e009ff30edbda5d965ef5bbf543
surajislam <154444093+surajislam@users.noreply.github.com>
2024-05-30 11:48:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,8 +41,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/shairy_k_dunia")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/RADHIKA_WORLD00")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "anokhikeduniya")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "anokhikeduniya")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -119,3 +119,51 @@ if SUPPORT_CHAT:
         raise SystemExit(
             "[ERROR] - Your SUPPORT_CHAT url is wrong. Please ensure that it starts with https://"
         )
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(getenv("API_ID", None))
+API_HASH = getenv("API_HASH", None)
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+OWNER_ID = int(getenv("OWNER_ID", "7178932244"))
+MONGO_URL = getenv("MONGO_URL", None)
+SUPPORT_GRP = getenv("SUPPORT_GRP", "pikachuworld0099")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "anokhikeduniya")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "@ll_SB_ANGLE_ll")
+
+# Random Start Images
+IMG = [
+    "https://graph.org/file/0dceb494bab6c37b8cd05.jpg",
+    "https://graph.org/file/a3db9af88f25bb1b99325.jpg",
+    "https://graph.org/file/5b344a55f3d5199b63fa5.jpg",
+    "https://graph.org/file/84de4b440300297a8ecb3.jpg",
+    "https://graph.org/file/84e84ff778b045879d24f.jpg",
+    "https://graph.org/file/a4a8f0e5c0e6b18249ffc.jpg",
+    "https://graph.org/file/ed92cada78099c9c3a4f7.jpg",
+    "https://graph.org/file/d6360613d0fa7a9d2f90b.jpg",
+]
+
+
+# Random Stickers
+STICKER = [
+    "CAACAgEAAxkBAAIJomRdLhVJVebkx0JRsp1STwTv3t3eAAJrAgAClpxhRD4z4bgqlIF0LwQ",
+    "CAACAgUAAxkBAAIJo2RdLhjLjCpmPipMT8ksrqwUjGAIAAK1BQACLZ8oVFVNmhalU8eOLwQ",
+    "CAACAgUAAx0CekRwCQADYWWaUnQ2oJqJLbDTZgZbXRT58wuqAAKECgACkgnhVLC0s4joaxF4HgQ",
+]
+
+
+EMOJIOS = [
+    "❤️",
+    "🧡",
+    "💛",
+    "💚",
+    "💙",
+    "💜",
+    "🤎",
+    "🖤",
+    "🤍",
+    "♥️",
+]
